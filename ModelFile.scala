530eae06ebe6418a7014f72d165ad7695af0b159
kdin <kannan.d@husky.neu.edu>
2017-04-22 21:46:33 -0400

    CV'd

--- ModelFile.scala
+++ ModelFile.scala
@@ -8,21 +8,57 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.mean
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg.Vectors
-/*import org.apache.spark.mllib.tree.RandomForest
-import org.apache.spark.mllib.tree.model.RandomForestModel
-import org.apache.spark.mllib.util.MLUtils*/
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.tuning.{ParamGridBuilder, CrossValidator}
 import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.ml.classification.{GBTClassificationModel, GBTClassifier}
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 object ModelFile {
-	def main(args: Array[String]) {
+
+def strippedLines(line: String) : String = {
+  var elements = line.split(",")
+  var one = line.split(",").slice(2,953)
+  var two = line.split(",").slice(955,1016)
+  var three = line.split(",").slice(1018, elements.length-1)
+  // elements.slice(2,953)
+  // elements.slice(955,1016)
+  // elements.slice(1018,elements.length-1)
+  var res = (one++two++three).mkString(",")
+  
+  return res
+}
+
+
+/*def getAccuracy(arg: (Double, Double)) = {
+  if (arg._1 == arg._2) {
+    accum.add(1)
+  }
+  total.add(1)
+}
+
+*/
+
+
+val corrected = functions.udf((value: Double) => value match {
+  case _ => if (value >= 1) 1 else 0
+  })
+
+val toDouble = functions.udf((value: String) => value match {
+  case _ => value.toDouble
+  })
+
+def main(args: Array[String]) {
 val conf = new SparkConf().setAppName("LoadDW")
+conf.set("spark.driver.maxResultSize", "3g")
 val sc = new SparkContext(conf)
 val sqlContext= new org.apache.spark.sql.SQLContext(sc)
-
+val accum = sc.longAccumulator("Accuracy")
+val total = sc.longAccumulator("Total")
 
 val spark = SparkSession
   .builder()
@@ -31,113 +67,150 @@ val spark = SparkSession
   .getOrCreate()
 // reading core variates:
 
-val corevar = sc.textFile("core-var")
-val columns = corevar.map(l=> l.split(":"))
-val feat= columns.map(token=> (token(0)))
-feat.collect().foreach(println)
-val featArr = feat.collect
-
-/*
-import sqlContext._
-import sqlContext.implicits._*/
-//Read file and create RDD
- /*val lines = sc.textFile(args(0))
- val parts = lines.map(line => line.replaceAll("\\?", ""))
-
-val schema = StructType(featArr.map(fieldName => StructField(fieldName, DoubleType, true)))
-val rowRDD = parts.map(r => Row.fromSeq(r))
 
 
-var df = spark.createDataFrame(rowRDD, schema)
-df.printSchema()
-df.show*/
 
-
-/*val extendedCore = sc.textFile("extended-core")
-val extendedColumns = extendedCore.map(l=> l.split(":"))
-val extendedFeat= extendedColumns.map(token=> (token(0)))
-val extFeatArr = extendedFeat.collect*/
-
-val lines = sc.textFile(args(0))
+/*val lines = sc.textFile(args(0))
 val colss = lines.first()
 val filtered = lines.filter(line => scala.util.Random.nextFloat < 0.005 || line.equals(colss))
-val parts = filtered.map(line => line.replaceAll("\\?", "0"))
-parts.coalesce(1).saveAsTextFile("OutputN")
+val parts = filtered.map(line=>line.replaceAll("\\?","0")).map(line=>line.replaceAll(", X",",1")).map(line=>line.replaceAll(",X",",1")).map(line => strippedLines(line))
+parts.saveAsTextFile(args(1))
+
+*/
 
 import sqlContext._
 import sqlContext.implicits._
-var df = load_a("OutputN",
+var df = load_a(args(1),
       sqlContext).cache()
 
-//df.select(df.columns.map(c => c.replaceAll("\\?", "")))
 
-/*
-val colsToRemove = Seq("SAMPLING_EVENT_ID0","LOC_ID1",  "SAMPLING_EVENT_ID1016", "LOC_ID1017", "SAMPLING_EVENT_ID953", "LOC_ID954") 
+//df.printSchema()
 
-val filteredDF = df.select(df.columns .filter(colName => !colsToRemove.contains(colName)) .map(colName => new Column(colName)): _*)
-//filteredDF.printSchema()
-val newDf = filteredDF.withColumnRenamed("SAMPLING_EVENT_ID0", "SAMPLING_EVENT_ID")
-val actualDf = newDf.withColumnRenamed("LOC_ID1", "LOC_ID")
-
-//actualDf.printSchema()
-println("array:"+ featArr.mkString(","))
-val selectedData = filteredDF.select(featArr.head, featArr.tail: _*)
-selectedData.printSchema()*/
-
-
-/*val colType  = df.dtypes
-colType.foreach(c=>println(c))*/
-//df = df.collect()
-/*colType.foreach(l=> imputed(l))*//*
-def imputed(col: (String,String)) = {
-  val mean1 = df.select(mean(col._1))*/
- // val meeaann = mean1.rdd
- // val mmmmmm= mean1.first()
-  //println("here::"+ mmmmmm.collect())
-  //println("mean:::"+mean1)
- // val mean1 = df.select(mean(col._1)).first()(0).asInstanceOf[Double]
- // println("mean:::"+mean1)
-  /*if (col._2.equals("IntegerType")) {
-    if (mean1 >= 0.5) 
-      df = df.na.fill(1, Seq(col._1))
-    else
-      df = df.na.fill(0, Seq(col._1)) 
-  }
-  else if (col._2.equals("DoubleType")) {
-    df = df.na.fill(mean1, Seq(col._1))   
-  }*/
-  //}
+val corevar = sc.textFile(args(2))
 
 
 
+val cols = corevar.map(l=> l.split(":")(0))
+cols.collect.foreach(col=>df =df.withColumn(col, toDouble(df(col))))
+df=df.withColumn("Agelaius_phoeniceus", corrected(df("Agelaius_phoeniceus")))
 
+
+val fields = df.schema.fields filter {
+x => x.dataType match { 
+      case x: org.apache.spark.sql.types.StringType => true
+      case _ => false 
+      } 
+    } map { x => x.name }
+
+val newDf = fields.foldLeft(df){ case(dframe,field) => dframe.drop(field) }
+
+df = newDf
+
+// df.printSchema()    
+
+val columns = corevar.map(l=> l.split(":"))
+val feat= columns.map(token=> (token(0)))
+var featArr = feat.collect
+
+val columnNames = featArr
+df = df.select(columnNames.head, columnNames.tail: _*)
+// df.printSchema()
+
+featArr = featArr.filter(line => !line.equals("Agelaius_phoeniceus"))
 
 
 
 val assembler = new VectorAssembler().setInputCols(featArr).setOutputCol("features")
 val df3 = assembler.transform(df)
-
+// df3.printSchema()
 val labelIndexer = new StringIndexer().setInputCol("Agelaius_phoeniceus").setOutputCol("label")
 val df4 = labelIndexer.fit(df3).transform(df3)
 
-val splitSeed = 5043 
-val Array(trainingData, testData) = df4.randomSplit(Array(0.7, 0.3), splitSeed)
+val splitSeed = 679
+val Array(trainingData, testData) = df4.randomSplit(Array(0.7, 0.3))
+
+
+
+
+/*
+val layers = Array[Int](66, 60, 55, 52, 47, 42, 37, 32, 27, 22, 17, 12, 7, 4, 2)
+
+// create the trainer and set its parameters
+val trainer = new MultilayerPerceptronClassifier()
+  .setLayers(layers)
+  .setTol(1E-10)
+  .setMaxIter(400)
+  .setLabelCol("label")
+  .setFeaturesCol("features")
+  .setBlockSize(128)
+  .setSeed(1234L)
+
+// train the model
+val model = trainer.fit(trainingData)*/
+
+/*
+val gbt = new GBTClassifier()
+  .setLabelCol("label")
+  .setFeaturesCol("features")
+  .setMaxIter(10)
+
+val model = gbt.fit(trainingData)*/
+
+
+/*
+val logisticRegressionClassifier = new LogisticRegression()
+                                      .setLabelCol("label")
+                                      .setFeaturesCol("features")
+                                      .setMaxIter(300)
+                                      .setTol(1E-10)
+val model = logisticRegressionClassifier.fit(trainingData)    */                                    
+
+
 val classifier = new RandomForestClassifier()
+  .setLabelCol("label")
+  .setFeaturesCol("features")
   .setImpurity("gini")
-  //.setMaxDepth(3)
-  .setNumTrees(10)
+  .setMaxBins(100)
+  .setNumTrees(30)
   .setFeatureSubsetStrategy("auto")
-  .setSeed(splitSeed)
-val model = classifier.fit(trainingData)
-val predictions = model.transform(testData)
-predictions.select("Agelaius_phoeniceus", "label", "prediction").show(5)
+ // .setSeed(splitSeed)
+
+val paramGrid = new ParamGridBuilder()
+                    .addGrid(classifier.maxDepth, Array(10,15,20,30))
+                    .build()
 val evaluator = new MulticlassClassificationEvaluator()
   .setLabelCol("label")
   .setPredictionCol("prediction")
-  .setMetricName("weightedPrecision")
+  // "f1" (default), "weightedPrecision", "weightedRecall", "accuracy"
+  .setMetricName("accuracy") 
+
+val pipeline = new Pipeline().setStages(Array(classifier))
+val cv = new CrossValidator()
+  .setEstimator(pipeline)
+  .setEvaluator(evaluator)
+  .setEstimatorParamMaps(paramGrid)
+  .setNumFolds(4)
+
+val model = cv.fit(trainingData)
+
+
+val predictions = model.transform(testData)
+
+val preds = predictions.select("label", "prediction")
+
+
+val labelAndPreds = preds.map { case Row(label:Double, prediction:Double) => (label, prediction)}
+val accr = labelAndPreds.filter(r => r._1 == r._2).count.toDouble/testData.count()
+
+println("ACCURACY ::::::: ", accr)
+/*val evaluator = new BinaryClassificationEvaluator()
+  .setLabelCol("label")
+  .setRawPredictionCol("prediction")
+  // .setMetricName("accuracy")
+
 val accuracy = evaluator.evaluate(predictions) 
 println("ACC:"+ accuracy)
-}
+*/}
 
 
  def load_a(path: String, sqlContext: SQLContext): DataFrame = {
@@ -145,7 +218,6 @@ println("ACC:"+ accuracy)
       .option("header", "true")
       .option("inferSchema", "true")
       .load(path)
-      //.toDF(featuresArr: _*)
     return data
   }
 }
